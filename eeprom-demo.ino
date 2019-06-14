4e606b9aa745ae4d7e2f7e64a160b74be12c30fb
Dong <donly@magicalboy.com>
2019-06-14 18:26:10 +0800

    add comments

--- eeprom-demo.ino
+++ eeprom-demo.ino
@@ -5,10 +5,12 @@ void setup() {
   Serial.begin(115200);
   EEPROM.begin(4); //  size being the number of bytes you want to use. 
 
+  // wirte demo
 //  EEPROM.write(0, 1);
 //  EEPROM.end();
 //  Serial.print("\nwirte end");
 
+  // read demo
   Serial.print("\nvalue=");
   byte value = EEPROM.read(0);
   Serial.print(value, DEC);
